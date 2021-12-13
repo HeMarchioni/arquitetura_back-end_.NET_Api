115d91b83c7d1910c0e97903265d0d4b1db67799
HeMarchioni <henrique.marchioni@fatec.sp.gov.br>
2021-12-13 11:08:17 -0300

    Configuração conexão com BD

---
+++ Curso.Api/Configurations/DbFactoryDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Curso.Api.Infraestruture.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Curso.Api.Configurations
+{
+    public class DbFactoryDbContext : IDesignTimeDbContextFactory<CursoDbContext>  //-> Classe para configuração da conexão com o banco (muitas vezes colocava no Startup)
+    {
+        public CursoDbContext CreateDbContext(string[] args)
+        {
+
+
+            var configuration = new ConfigurationBuilder()           //-> para usar a string de conexão que esta no appsettings
+                                    .AddJsonFile("appsettings.json")
+                                    .Build();
+
+
+            var optionsBuilder = new DbContextOptionsBuilder<CursoDbContext>();    
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            CursoDbContext contexto = new CursoDbContext(optionsBuilder.Options);
+            return contexto;
+
+
+
+        }
+    }
+}
